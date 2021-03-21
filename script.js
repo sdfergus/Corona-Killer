accd2a0b764fe4510a0b6d9396fca524ccbed3b7
Shandira <sduxe23@gmail.com>
2021-03-20 21:13:52 -0700

    needle shooter left-right arrow functions done - modern browsers in 2019

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 'use strict';
 
-let element = document.getElementById('needle-shooter-img');
+const element = document.getElementById('needle-shooter-img');
+const startLabel = document.querySelector('.start-label');
+
+//* Init game
 element.style.left = element.style.left + 48 + '%'; //Initalize starting needle shooter position
 
+/* Needle shooter functionality */
 function leftArrowPressed() {
+  startLabel.classList.add('hidden'); //Hide start label on left key press
   element.style.left = parseInt(element.style.left) - 1 + '%'; //Reassigns left style (shifting needle by 1% to left)
 }
 
 function rightArrowPressed() {
+  startLabel.classList.add('hidden'); //Hide start label on right key press
   element.style.left = parseInt(element.style.left) + 1 + '%'; //Reassigns left style (shifting needle by 1% to right)
 }
 
 function moveSelection(event) {
-  switch (event.keyCode) {
-    case 37:
+  switch (event.code) {
+    case 'ArrowLeft':
       leftArrowPressed();
       break;
-    case 39:
+    case 'ArrowRight':
       rightArrowPressed();
       break;
   }
